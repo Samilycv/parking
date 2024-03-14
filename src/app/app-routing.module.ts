3d62279f68d300a97e327a28ac46ac648565d403
samily <samilycv@gmail.com>
2024-03-14 10:48:38 +0530

    parkingproject1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:HomeComponent
+  },
+  {
+    path:"login",component:LoginComponent
+  },
+   {
+    path:"adminlogin",component:AdminloginComponent
+  },
+  {
+    path:"register",component:RegisterComponent
+  },
+  {
+    path:"*", component:PagenotfoundComponent
+  },
+  
+
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
